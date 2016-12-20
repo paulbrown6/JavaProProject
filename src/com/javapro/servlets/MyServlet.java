d798fca3da2873466c2df34e4043310ac6ab9c54
Paul Brown <paulbrown6@mail.ru>
2016-12-20 19:03:45 +0300

    Update 1

---
+++ src/com/javapro/servlets/MyServlet.java
@@ -0,0 +1,25 @@
+package com.javapro.servlets;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Paul Brown on 20.12.2016.
+ */
+public class MyServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
