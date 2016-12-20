00210d704fe812299b7a2c0ca10d8aefb223446b
Paul Brown <paulbrown6@mail.ru>
2016-12-20 20:14:43 +0300

    Update: add web.

--- src/com/javapro/servlets/MyServlet.java
+++ src/com/javapro/servlets/MyServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Paul Brown on 20.12.2016.
@@ -15,6 +16,8 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
+        PrintWriter out = resp.getWriter();
+        out.print("<h1>У меня получилось сделать это!</h1>");
     }
 
     @Override
