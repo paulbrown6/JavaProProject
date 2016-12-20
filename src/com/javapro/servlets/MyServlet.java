909df7602d0fc95ee85641359cd070451407136f
Paul Brown <paulbrown6@mail.ru>
2016-12-20 19:08:35 +0300

    Update 1

--- src/com/javapro/servlets/MyServlet.java
+++ src/com/javapro/servlets/MyServlet.java
@@ -1,6 +1,5 @@
 package com.javapro.servlets;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
